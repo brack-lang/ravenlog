1aa1f8fcd4cae890da92215ac69e7d375ce65790
Mutsuha Asada <me@momee.mt>
2024-12-31 18:05:23 +0900

    feat: deploy action

---
+++ actions/deploy/deploy.sh
@@ -0,0 +1,18 @@
+set -eou pipefail
+
+cd $WORKING_DIRECTORY
+nix run "github:brack-lang/ravenlog?dir=backend" build
+git clone https://github.com/brack-lang/ravenlog
+mkdir workspace
+mv ravenlog/frontend/* workspace
+cp .ravenlog/blog_settings.json workspace/src/app/_assets/
+cp .ravenlog/posts.json workspace/src/app/_assets/
+cp -r .ravenlog/assets/** workspace/public/
+
+grep -v '^workspace/$' | grep -v '^\.git$' | xargs rm -rf
+mv workspace/* .
+rm -rf workspace
+git switch -c $DEPLOY_BRANCH
+git add *
+git commit -m "Deploy"
+git push origin $DEPLOY_BRANCH --force
