2f94ac90ca2004b0a3a06e168a3e21cc2a8f1385
Mutsuha Asada <me@momee.mt>
2025-03-14 11:42:00 +0900

    fix: heading style

--- frontend/src/app/_ravenlog_components/Headings/index.tsx
+++ frontend/src/app/_ravenlog_components/Headings/index.tsx
@@ -5,25 +5,43 @@ type Props = {
 };
 
 export const Heading1 = ({ children }: Props) => {
-  return <h1 className="ravenlog_heading1">{children}</h1>;
+  return <h1 className="ravenlog_heading1">
+    <span className="sharp">#</span>
+    {children}
+  </h1>;
 };
 
 export const Heading2 = ({ children }: Props) => {
-  return <h2 className="ravenlog_heading2">{children}</h2>;
+  return <h2 className="ravenlog_heading2">
+    <span className="sharp">##</span>
+    {children}
+  </h2>;
 };
 
 export const Heading3 = ({ children }: Props) => {
-  return <h3 className="ravenlog_heading3">{children}</h3>;
-};
+  return <h3 className="ravenlog_heading3">
+    <span className="sharp">###</span>
+    {children}
+  </h3>;
+}
 
 export const Heading4 = ({ children }: Props) => {
-  return <h4 className="ravenlog_heading4">{children}</h4>;
-};
+  return <h4 className="ravenlog_heading4">
+    <span className="sharp">####</span>
+    {children}
+  </h4>;
+}
 
 export const Heading5 = ({ children }: Props) => {
-  return <h5 className="ravenlog_heading5">{children}</h5>;
-};
+  return <h5 className="ravenlog_heading5">
+    <span className="sharp">#####</span>
+    {children}
+  </h5>;
+}
 
 export const Heading6 = ({ children }: Props) => {
-  return <h6 className="ravenlog_heading6">{children}</h6>;
-};
+  return <h6 className="ravenlog_heading6">
+    <span className="sharp">######</span>
+    {children}
+  </h6>;
+}
